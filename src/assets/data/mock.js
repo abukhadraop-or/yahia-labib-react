3621e86db2089218e0f66e242464bd0a83c9d202
yahialabeeb <yahialabeeb.gmail.com>
2022-04-13 18:00:52 +0300

    add 2 test for movies list component

--- src/assets/data/mock.js
+++ src/assets/data/mock.js
@@ -1,4 +1,4 @@
-const mockData = {
+export const mockData = {
   results: [
     {
       id: 675353,
@@ -30,4 +30,35 @@ const mockData = {
   ],
 };
 
-export default mockData;
+export const mockData2 = {
+  results: [
+    {
+      id: 63,
+      overview:
+        'After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero.',
+      poster_path: '/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg',
+      release_date: '2022-03-30',
+      title: 'Sonic the Hedgehog 2',
+      vote_average: 7.7,
+    },
+    {
+      id: 69,
+      overview:
+        'Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.',
+      poster_path: '/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg',
+      release_date: '2021-12-15',
+      title: 'Spider-Man: No Way Home',
+      vote_average: 8.2,
+    },
+    {
+      id: 57,
+      overview:
+        'Thirteen-year-old Mei is experiencing the awkwardness of being a teenager with a twist – when she gets too excited, she transforms into a giant red panda.',
+      poster_path: '/qsdjk9oAKSQMWs0Vt5Pyfh6O4GZ.jpg',
+      release_date: '2022-03-10',
+      title: 'Turning Red',
+      vote_average: 7.5,
+    },
+  ],
+};
+
--- src/components/MovieList/MovieList.test.jsx
+++ src/components/MovieList/MovieList.test.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import MoviesList from 'components/MovieList/MoviesList';
 import '@testing-library/jest-dom/extend-expect';
-import mockData from 'assets/data/mock';
-import { render, screen } from '@testing-library/react';
+import { mockData, mockData2 } from 'assets/data/mock';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { MovieContextProvider } from 'context/MoviesContext';
 
 describe('MoviesList', () => {
+  afterEach(() => {
+    cleanup();
+  });
   test('should render movies', async () => {
     window.fetch = jest.fn();
     window.fetch.mockResolvedValueOnce({
@@ -18,5 +21,37 @@ describe('MoviesList', () => {
     );
     const elements = await screen.findAllByRole('listitem');
     expect(elements.length).toBe(3);
+  });
+
+  test('should render More Movie button', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => mockData,
+    });
+    render(
+      <MovieContextProvider>
+        <MoviesList />
+      </MovieContextProvider>
+    );
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => mockData2,
+    });
+    const btnElement = screen.getByRole('button');
+    fireEvent.click(btnElement);
+    const elements = await screen.findAllByRole('listitem');
+    expect(elements.length).toBeGreaterThan(4);
+  });
+  test('should not render movies', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => ({ results: [] }),
+    });
+    render(
+      
+        <MoviesList />
+    );
+    const element = screen.queryAllByRole('listitem');
+    expect(element.length).toBe(0);
   });
 });
